8bf9b3d9e82eac473cdc844ef61be8adb816c8ff
ATRI2107 <atrichaturvedi@gmail.com>
2020-10-19 20:54:20 +0530

    Added the classes as modules

--- app.java
+++ app.java
@@ -1,79 +1,14 @@
-import clients.clients;
 import java.util.*;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.text.SimpleDateFormat;
 import java.io.*;
-/*class clients
-{
-    int client_id;
-    String name;
-    String address;
-    String suburb;
-    String state;
-    int postcode;
-    clients(int client_id,String name,String address,String suburb,String state,int postcode)
-    {
-        this.client_id=client_id;
-        this.name=name;
-        this.address=address;
-        this.suburb=suburb;
-        this.state=state;
-        this.postcode=postcode;
-    }
-}*/
+import clients.*;
+import properties.*;
+import expenses.expenses;
+import rents.*;
 
-class properties
-{
-    int property_id;
-    String address;
-    String suburb;
-    String state;
-    int postcode;
-    int rent;
-    double fee;
-    int client_id;
-    properties(int property_id,String address,String suburb,String state,int postcode,int rent,double fee,int client_id)
-    {
-        this.property_id=property_id;
-        this.address=address;
-        this.suburb=suburb;
-        this.state=state;
-        this.postcode=postcode;
-        this.rent=rent;
-        this.fee=fee;
-        this.client_id=client_id;
-    }
-}
-
-class expenses
-{
-    int property_id;
-    String expense;
-    double cost;
-    String date;
-    expenses(int property_id,String expense,double cost,String date)
-    {
-        this.property_id=property_id;
-        this.expense=expense;
-        this.cost=cost;
-        this.date=date;
-    }
-}
-
-class rents
-{
-    int property_id;
-    int rent_amt;
-    String date;
-    rents(int property_id,int rent_amt,String date)
-    {
-        this.property_id=property_id;
-        this.rent_amt=rent_amt;
-        this.date=date;
-    }
-}
 class PropertyReport
 {
     HashMap<Integer,properties> prop2=new HashMap<>();
